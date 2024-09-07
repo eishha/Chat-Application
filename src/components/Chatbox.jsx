3062377c6d89ec9de29998269d4b7679b60c2110
eishha <eishhamaqbool@gmail.com>
2024-09-07 15:25:10 +0500

    Fixed chatbox

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -8,6 +8,13 @@ import { Box } from '@chakra-ui/react'
 const ChatBox = () => {
   const [messages, setMessages] = useState([])
 
+  const scroll = useRef(null)
+  const scrollToBottom = () => {
+    scroll.current.scrollIntoView({ behavior: 'smooth' })
+  }
+  useEffect(() => {
+    scrollToBottom()
+  }, [messages])
   useEffect(() => {
     const q = query(collection(db, 'messages'), orderBy('createdAt', 'desc'))
 
@@ -34,6 +41,7 @@ const ChatBox = () => {
         {messages?.map((message) => (
           <Message key={message.id} message={message} />
         ))}
+        <div ref={scroll}></div>
       </Box>
 
       <SendMessage />
